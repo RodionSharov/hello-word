ddb89d52264d0ce813a2d42639ad49d19d727a1f
RodionSharov <75029338+RodionSharov@users.noreply.github.com>
2020-11-30 09:18:32 +0300

    Update House_Drawing.py

--- House_Drawing.py
+++ House_Drawing.py
@@ -1,32 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 29 23:05:03 2020
-
-Calendar
-
-@author: Родион
-"""
 import turtle as t
 t.hideturtle()
-
+t.speed(10)
 class House:
-    def __init__(self,X,Y):
-        global X_Main
-        X_Main = X
-        global Y_Main
-        Y_Main = Y
+    X_Main = 0
+    Y_Main = 0
+    def Locate(self,X,Y):
+        House.X_Main = X
+        House.Y_Main = Y
+    
     def First_move(self):
         t.penup()
-        t.setpos(X_Main,Y_Main)
+        t.setpos(House.X_Main,House.Y_Main)
         t.pendown()
   
    
-class Walls():
+class Walls:
     def __init__(self,ww,wh,color="Red"):
-        self.X_Walls = X_Main 
-        self.Y_Walls = Y_Main
-        self.Walls_Width = abs(X_Main)* ww 
-        self.Walls_Height = abs(Y_Main)* wh 
+        self.X_Walls =  House.X_Main
+        self.Y_Walls = House.Y_Main
+        self.Walls_Width = abs(self.X_Walls)* ww 
+        self.Walls_Height = abs(self.Y_Walls)* wh 
         self.Walls_Color = color
         
     def Draw_Walls(self):
@@ -39,18 +32,20 @@ class Walls():
         t.forward(self.Walls_Width)
         t.end_fill()
         t.penup()
- 
+
+
         
    
     
-X = -200
-Y = -200
+X = -300
+Y = -100
 
 
 
-House_Object = House(X,Y)       
+House_Object = House()
+House_Object.Locate(X,Y)       
 House_Object.First_move()
-Walls_Obj = Walls(2,1,color = "Blue")
+Walls_Obj = Walls(1.2,1,color = "Green")
 Walls_Obj.Draw_Walls()
 
 
